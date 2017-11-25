82687e64ba4ead80f84802235a6d2d07d957d047
Gonso <val.gonsolin@gmail.com>
2017-11-25 19:35:30 +0000

    Fichiers de bases, ajout du favicon

---
+++ base.php
@@ -0,0 +1,33 @@
+<?php
+function importEssential(){ ?>
+  <!doctype html>
+
+
+  <html lang="fr">
+  <head>
+
+      <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+      <meta name="viewport" content="width=device-width, initial-scale=0.635, user-scalable=yes"/>
+      <meta name="apple-mobile-web-app-capable" content="yes"/>
+      <meta name="robots" content="noindex, nofollow, noarchive"/>
+      <link rel="shortcut icon" type="image/x-icon" href="/images/favicon.png"/>
+
+
+      <link rel="stylesheet" href="/bootstrap/css/bootstrap.css"/>
+      <script src="/js/jquery-3.2.1.min.js"></script>
+      <link rel="stylesheet" href="/css/bootstrap.min.css">
+      <script src="/js/bootstrap.min.js"></script>
+
+
+
+
+      <link rel="stylesheet" href="/css/base.css"/>
+
+
+      <title>CSRT - Formula Student</title>
+
+
+  </head>
+
+<?php
+} ?>
--- index.php
+++ index.php
@@ -1,2 +1,4 @@
+<?php include_once "base.php";
+importEssential(); ?>
 <p>SAlut CSRT</p>
 Brackets
